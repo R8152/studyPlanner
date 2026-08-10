45b90a5fa7d78e55e3fdf9bae3e8eb7b6360bd67
R8152 <raulteles05@gmail.com>
2026-08-10 19:48:08 -0300

    feat: create side bar

---
+++ studyPlanner/src/components/SideBar.jsx
@@ -0,0 +1,23 @@
+import {BookOpenTextIcon} from "@phosphor-icons/react"
+import "./SideBar.css"
+export default function SideBar(){
+    return (
+        <>
+        <aside>
+            <div>
+                <BookOpenTextIcon size={32} color="#094cb2" />
+                <div>
+                    <label>Silk Study</label>
+                    <span>Manager</span>
+                </div>
+            </div>
+            <div>
+
+            </div>
+            <div>
+
+            </div>
+        </aside>
+        </>
+    )
+}
