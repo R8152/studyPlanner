3d9f3c4c17211bb9f51461c1a70b089e1a01525e
R8152 <raulteles05@gmail.com>
2026-08-15 11:54:06 -0300

    style: estiliza LoginPage

--- studyPlanner/src/pages/LoginPage.jsx
+++ studyPlanner/src/pages/LoginPage.jsx
@@ -1,9 +1,32 @@
-
+import './LoginPage.css'
 
 export default function LoginPage() {
   return (
     <>
-      <h1>Login Page</h1>
+        <div className='LoginPage'>
+            <label className='SilkStudy'>Silk Study</label>
+            <h2 className='welcoming'>Welcome Back.</h2>
+            <span className='welcoming2'>Please enter your details to access your curated library.</span>
+            <main className='LoginInfo'>
+                <div>
+                    <h3 className='email'>Email Address</h3>
+                    <input type="email" name="" id="" placeholder='scholar@silkstudy.edu' className='input'/>
+                </div>
+                <div>
+                    <div className='password'>
+                        <h3 className='password-title'>Password</h3>
+                        <span className='forgotpassword'>Forgot Password?</span>
+                    </div>
+                    <input type="password" name="" id="" placeholder='••••••••' className='input'/>
+                </div>
+                <div className='div-checkbox'>
+                    <input type="checkbox" name="" id="" className='checkbox'/>
+                    <span className='loggedin'>Keep me logged in</span>
+                </div>
+                <button className='entrar'>Entrar</button>
+            </main>
+        </div>
+
     </>
   )
 }
