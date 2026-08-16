da970bd42168f2b492d4668024963fd27a9e7a48
R8152 <raulteles05@gmail.com>
2026-08-16 16:26:26 -0300

    style: estiliza register page

--- studyPlanner/src/pages/RegisterPage.jsx
+++ studyPlanner/src/pages/RegisterPage.jsx
@@ -1,9 +1,23 @@
+import "./RegisterPage.css";
+import Library from "../assets/library.png";
 export default function RegisterPage() {
   return (
-    <>
-      <h1>RegisterPage</h1>
-    </>
+    <div className="RegisterPage">
+      <main className="propaganda">
+        <img src={Library} alt="" className="imgpropaganda"/>
+        <div className='infopropaganda'>
+          <h1 className="titlepropaganda">The Digital Curator</h1>
+          <span className="textpropaganda">A scholarly, premium reading experience. Dense information made<p>effortless through serif authority and generous whitespace.</p></span>
+        </div>
+      </main>
+      <main className="info-register">
+        <div className="voltar">
+          <span>← Voltar</span>
+        </div>
+        <div>
+          <h1>Silk Study</h1>
+        </div>
+      </main>
+    </div>
   )
 }
-
-
