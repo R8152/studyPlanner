1abb211bcf185ab511a849379ed662cce96d0ac5
R8152 <raulteles05@gmail.com>
2026-08-18 21:09:06 -0300

    style: continua estilização de register page

--- studyPlanner/src/pages/RegisterPage.jsx
+++ studyPlanner/src/pages/RegisterPage.jsx
@@ -1,4 +1,5 @@
 import "./RegisterPage.css";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import Library from "../assets/library.png";
 export default function RegisterPage() {
   return (
@@ -14,9 +15,27 @@ export default function RegisterPage() {
         <div className="voltar">
           <span>← Voltar</span>
         </div>
-        <div>
-          <h1>Silk Study</h1>
+        <div className="formulario">
+          <h1 className="title-formulario">Silk Study</h1>
+          <span className="p-formulario">Inicie sua jornada acadêmica.</span>
+          <div className="info1">
+            <h2 className="h2-info">Nome completo</h2>
+            <input type="text" placeholder="Seu nome completo" className="inputinfo"/>
+          </div>
+          <div className="info2">
+            <h2 className="h2-info">email</h2>
+            <input type="email" placeholder="seu@email.com" className="inputinfo"/>
+          </div>
+          <div className="info3">
+            <h2 className="h2-info">senha</h2>
+            <input type="password" placeholder="Mínimo de 8 caracteres" className="inputinfo"/>
+          </div>
+          <div className="info4">
+            <h2 className="h2-info">confirmar senha</h2>
+            <input type="password" placeholder="Repita sua senha" className="inputinfo"/>
+          </div>
         </div>
+        <span>Criar Conta <FontAwesomeIcon icon="fa-solid fa-arrow-right-long" /></span>
       </main>
     </div>
   )
